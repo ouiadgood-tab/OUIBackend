acf1e72656e4448992e28197496433170971c0ea
Emmanuel Amadi <emmanuelamadi111@gmail.com>
2023-05-19 03:43:34 -0700

    update

--- OUIBackend/settings.py
+++ OUIBackend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-92g7%p%yyih%l@@nfp7b4tj41(bgjq=wo5jh9&9u(a%)h=x)j4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['oui-backend.herokuapp']
 
 
 # Application definition
